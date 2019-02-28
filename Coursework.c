c8a9e5e4fea564d86ec2bb193a1ca2f408e5fa5f
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2019-02-28 16:02:18 +0000

    lab 1 changes, added lots

--- Coursework.c
+++ Coursework.c
@@ -1,43 +1,101 @@
 #include <stdio.h>
-Board init_board(){
-	Board b;
+#include <stdbool.h>
+
+struct Row{
+	char row[3];
+};
+
+struct Board{
+	struct Row Positions[3];	
+};
+struct Board init_board(){
+	struct Board b;
+	
+	for(int i = 0; i<3; i++){
+		for (int j = 0; j<3; j++){
+			b.Positions[i].row[j]=' ';
+		}
+	}
 	return b;
 	
 }
-
-
-int main(int argc, char **argv)
-{
-	struct Board{
-		Row Positions[3];
-		
-	};
-	struct Row{
-		char row[3];
-	};
-	
+void print_board(struct Board b){
 	int boardsize = 3;
 	int count = 0;
-	printf("Welcome to Tic tac toe!\n");
 	while(count != boardsize){
-		for(int i = 2; i<boardsize; i++){
-			printf("  |");
+		for(int i = 0; i<boardsize-1; i++){
+			printf(" %c|", b.Positions[i].row[count]);
 		}
-		printf("  |\n");
-		
+		printf(" %c\n",b.Positions[2].row[count]);
 		if(boardsize - count == 1){
-			printf("\n\nStart Game!\n");
-			
+			printf("\n\nBoard Updated\n");
 		}else{
-			
-			for(int i = 2; i<boardsize; i++)
+			for(int i = 0; i<boardsize-1; i++)
 			{
-				printf("----");
+				printf("---");
 			}
-			printf("----\n");
+			printf("---\n");
 		}
 		count++;
 	}
-	Board b = init_board();
 	
+	
+}
+
+bool check_victory(struct Board b){
+	bool won = false;
+	
+	
+	return won;
+}
+char change_turn(char turn){
+	if(turn == 'X'){
+		turn = 'O';
+	}else{
+		turn = 'X';
+	}
+	return turn;
+}
+int main(int argc, char **argv)
+{
+	int row;
+	int col;
+	int turncount = 0;
+	char turn = 'X';
+	bool valid = false;
+	bool won = false;
+	printf("Welcome to Tic tac toe!\n");
+	struct Board b = init_board();
+	print_board(b);
+	while(!won){
+		while(!valid){
+			printf("%c, your turn!\nrow:", turn);
+			scanf("%d", &row);
+			printf("column:");
+			scanf("%d", &col);
+			if(b.Positions[col-1].row[row-1] != ' '){
+				printf("error place already full\n");
+			}else{
+				b.Positions[col-1].row[row-1] = turn;
+				valid = true;
+			}
+			print_board(b);
+		}
+	won = check_victory(b);
+	valid = false;
+	turn = change_turn(turn);
+	turncount++;
+	if(turncount == 9){
+		
+		printf("Stalemate!\nPlay again? (y/n):");
+		char decicion;
+		scanf("%c", &decicion);
+		if(decicion == 'y'){
+			turncount = 0;
+			b = init_board();
+		}else{
+			won = true;
+		}
+	}
+	}
 }
