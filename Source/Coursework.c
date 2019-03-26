ce8920e0f4f815d42b1377ea053fce8bf43a9c27
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2019-03-26 16:08:05 +0000

    final version....?
    hopefully?

--- Source/Coursework.c
+++ Source/Coursework.c
@@ -19,6 +19,12 @@ struct Board init_board(){
 	return b;
 	
 }
+
+struct Replay{
+	int numofmoves;
+	struct Board States[9];
+};
+
 void print_board(struct Board b){
 	int boardsize = 3;
 	int count = 0;
@@ -38,8 +44,13 @@ void print_board(struct Board b){
 		}
 		count++;
 	}
-	
-	
+}
+
+void play_replay(struct Replay r){
+	for(int i = 0; i < r.numofmoves; i++){
+		print_board(r.States[i]);
+	}
+	printf("\nEnd of replay\n");
 }
 
 bool check_victory(struct Board b, char *winner){
@@ -77,56 +88,137 @@ int main(int argc, char **argv)
 	int row;
 	int col;
 	int turncount = 0;
+	int replaycount = 0;
+	int viewnumber;
 	char turn = 'X';
 	char decicion;
-	char winner;
+	char winner = '-';
 	bool valid = false;
 	bool won = false;
+	bool done = false;
+	bool viewing = true;
 	printf("Welcome to Tic tac toe!\n");
 	struct Board b = init_board();
-	while(!won){
-		while(!valid){
-			print_board(b);
-			row = 0;
-			col = 0;
-			printf("%c, your turn!\nrow:", turn);
-			while(scanf("%d", &row)!= 1 ){
-				printf("Invalid data\nrow:");
+	struct Replay r;
+	struct Replay List[20];
+	while(!done){
+		while(!won){
+			while(!valid){
+				print_board(b);
+				row = 0;
+				col = 0;
+				printf("%c, your turn!\nrow:", turn);
+				while(scanf("%d", &row)!= 1 ){
+					printf("Invalid data\nrow:");
+					getchar();
+				}
+				
+				if (row > 3){valid = false;}
+				printf("column:");
+				while(scanf("%d", &col) !=1){
+					printf("Invalid data\nrow:");
+					getchar();
+				}
 				getchar();
+				if (col > 3){valid = false;}
+				if(b.Positions[col-1].row[row-1] != ' '){
+					printf("error place already full\n");
+				}else{
+					b.Positions[col-1].row[row-1] = turn;
+					valid = true;
+				}
 			}
-			
-			if (row > 3){valid = false;}
-			printf("column:");
-			while(scanf("%d", &col) !=1){
-				printf("Invalid data\nrow:");
-				getchar();
+			r.States[turncount] = b;
+			won = check_victory(b, &winner);
+			if(!won){valid = false;}
+			turn = change_turn(turn);
+			turncount++;
+			if(turncount == 9 && !won){
+				print_board(b);
+				printf("Stalemate!\nPlay again or view replays? (y/n/r):");
+				if(decicion == '\n'){scanf("%c", &decicion);} //clearing decision because bugs and stuff
+				scanf("%c", &decicion);
+				if(decicion == 'y'){
+					if(replaycount<20){
+						r.numofmoves = turncount;
+						List[replaycount] = r;
+						replaycount = replaycount + 1;
+					} else{
+						printf("Replay Storage full, please restart program if you want to save different replays\n");
+					}
+					turncount = 0;
+					b = init_board();
+					turn = 'X';
+				}else if(decicion == 'r'){
+					if(replaycount<20){
+						r.numofmoves = turncount;
+						List[replaycount] = r;
+						replaycount = replaycount + 1;
+					} else{
+						printf("Replay Storage full, please restart program if you want to save different replays\n");
+					}
+					viewing = true;
+					while(viewing){
+						printf("\nEnter the number of replay to display: ");
+						scanf("%d", &viewnumber);
+						if(viewnumber-1 >= replaycount || viewnumber == 0){
+							printf("Error, replay not found");
+						}else{
+							play_replay(List[viewnumber-1]);
+						printf("Continue viewing replays? (y/n):");
+						if(decicion == '\n'){scanf("%c", &decicion);} //clearing decision because bugs and stuff
+						scanf("%c", &decicion);
+						if(decicion == 'n'){viewing = false;}
+						}
+					}
+					turncount = 0;
+					b = init_board();
+					turn = 'X';
+				}else{
+					won = true;
+					done = true;
+				}
 			}
-			getchar();
-			if (col > 3){valid = false;}
-			if(b.Positions[col-1].row[row-1] != ' '){
-				printf("error place already full\n");
-			}else{
-				b.Positions[col-1].row[row-1] = turn;
-				valid = true;
-			}
-			
 		}
-		won = check_victory(b, &winner);
-		if(!won){valid = false;}
-		turn = change_turn(turn);
-		turncount++;
-		if(turncount == 9 && !won){
-			print_board(b);
-			printf("Stalemate!\nPlay again? (y/n):");
-			scanf("%c", &decicion);
-			if(decicion == 'y'){
-				turncount = 0;
-				b = init_board();
-			}else{
-				won = true;
+		print_board(b);
+		if(replaycount<20){
+			r.numofmoves = turncount;
+			List[replaycount] = r;
+			replaycount = replaycount + 1;
+		} else{
+			printf("Replay Storage full, please restart program if you want to save different replays\n");
+		}
+		printf("%c is the winner!", winner);
+		printf("\nPlay again or view replays? (y/n/r):");
+		if(decicion == '\n'){scanf("%c", &decicion);} //clearing decision because bugs and stuff
+		scanf("%c", &decicion);
+		if(decicion == 'y'){
+			turncount = 0;
+			b = init_board();
+			turn = 'X';
+			won = false;
+		}else if(decicion == 'r'){
+			viewing = true;
+			while(viewing){
+				printf("\nEnter the number of replay to display: ");
+				scanf("%d", &viewnumber);
+				if(viewnumber-1 >= replaycount || viewnumber == 0){
+					printf("Error, replay not found");
+				}else{
+					play_replay(List[viewnumber-1]);
+					printf("Continue viewing replays? (y/n):");
+					if(decicion == '\n'){scanf("%c", &decicion);} //clearing decision because bugs and stuff
+					scanf("%c", &decicion);
+					if(decicion == 'n'){viewing = false;}
+				}
 			}
+			turncount = 0;
+			b = init_board();
+			turn = 'X';
+			won = false;
+		}else{
+			done = true;
 		}
 	}
-	print_board(b);
-	printf("%c is the winner!", winner);
+	
 }
